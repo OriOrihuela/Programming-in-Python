4cc09e694059b244a82687f4fe5bfda43be26308
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-12-09 21:52:55 +0100

    Homework updated

--- Binary_research.py
+++ Binary_research.py
@@ -1,17 +1,37 @@
-int numeros = {1,2,3,4,5,6,7,8,9}
-int posIzquierda, posDerecha, posCentral
-int numeroBuscado
 
-def busquedaBinaria(lista, numeroBuscado)
-    posIzquierda = numeros[0]
-    posDerecha = len(numeros) - 1
+'''Binary Research of a number within an array'''
+
+def binaryResearch(array, numeroBuscado):
+    
+    posIzquierda = 0
+    posDerecha = len(array) - 1
+    
     while posIzquierda <= posDerecha:
-        posCentral = (posIzquierda+posDerecha) / 2
-        if numeros[posCentral] == numeroBuscado:
+        
+        posCentral = int((posIzquierda + posDerecha) / 2)
+        
+        if array[posCentral] == numeroBuscado:
             return posCentral
-        elif numeros[posCentral] > numeroBuscado:
+        
+        elif array[posCentral] > numeroBuscado:
             posDerecha = posCentral - 1
+        
         else:
             posIzquierda = posCentral + 1
+    
     return - 1
-print def busquedaBinaria(numeros, 8)
+
+
+if __name__ == "__main__":
+    
+    # TEST CASES# 
+
+    array1 = [1,2,3,4,5,6,7,8,9]
+    assert binaryResearch(array1, 8) == 7
+
+    array2 = [2,4,6,8,10,12,14,16,18,20]
+    assert binaryResearch(array2, 6) == 2
+
+    array3 = [43,56,12,11,8,97,100,67]
+    assert binaryResearch(array3, 97) == 5
+
