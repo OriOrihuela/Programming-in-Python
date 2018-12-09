b3ba0d15d447f7035707e7960b6e4698ee8698ef
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-12-09 21:08:25 +0100

    Homewrok updated

--- num_is_prime.py
+++ num_is_prime.py
@@ -15,54 +15,15 @@ if __name__ == "__main__":
 
     # CASOS TEST #
 
-    if isPrime(6):
-        print("6 Is prime")
-    else:
-        print("6 is not prime")
-
-    if isPrime(73):
-        print("73 is prime")
-    else:
-        print("73 is not prime")
-
-    if isPrime(564):
-        print("564 is prime")
-    else:
-        print("564 is not prime")
-
-    if isPrime(11):
-        print("11 is prime")
-    else:
-        print("11 is not prime")
-
-    if isPrime(67):
-        print("67 is prime")
-    else:
-        print("67 is not prime")
-
-    if isPrime(88):
-        print("88 is prime")
-    else:
-        print("88 is not prime")
-
-    if isPrime(233):
-        print("233 is prime")
-    else:
-        print("233 is not prime")
-
-    if isPrime(1):
-        print("1 is prime")
-    else:
-        print("1 is not prime")
-
-    if isPrime(2):
-        print("2 is prime")
-    else:
-        print("2 is not prime")
-
-    if isPrime(0):
-        print("0 is prime")
-    else:
-        print("0 is not prime")
+    assert not isPrime(6)
+    assert isPrime(73)
+    assert not isPrime(564)
+    assert isPrime(11)
+    assert isPrime(67)
+    assert not isPrime(88)
+    assert isPrime(233)
+    assert not isPrime(1)
+    assert isPrime(2)
+    assert not isPrime(0)
 
     
