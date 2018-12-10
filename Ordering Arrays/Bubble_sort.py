fde52b1e667c2195b4f4508e1883ab807b2b5448
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-10 10:44:06 +0100

    Update Bubble_sort.py

--- Ordering Arrays/Bubble_sort.py
+++ Ordering Arrays/Bubble_sort.py
@@ -1,14 +1,5 @@
 
-'''import matplotlib.pyplot as plt
-from random import shuffle
 
-def createRandomList(length):
-    lista = [number for number in range(length)]
-    # shuffle no devuelve nada: cambia la lista in place
-    # porque las listas son objetos mutables
-    shuffle(lista)
-    assert len(lista) == length
-    print(lista)'''
 
 def bubblesort(lista):
     recorrer_array = True
@@ -31,9 +22,6 @@ if __name__ == "__main__":
     
     
     # TEST CASES # 
-
-
-    '''lista = createRandomList(15)'''
 
     
     assert bubblesort([13,76,54,34,23,67,89]) == [13,23,34,54,67,76,89]
