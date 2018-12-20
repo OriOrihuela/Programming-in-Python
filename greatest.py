754a6e364fa680e2184e42d5d4aa8854a62d64c4
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 15:21:17 +0100

    Update and rename Greatest.py to greatest.py

--- greatest.py
+++ greatest.py
@@ -13,25 +13,10 @@ def greatest(lista):
         return max(lista)
 
 
-
-
 if __name__ == "__main__":
 
-    print(greatest([4,23,1]))
-    #>>>23
-
-
-    print(greatest([4,23,1,50]))
-    #>>>50
-
-
-    print(greatest([67,99,1,5,200]))
-    #>>>200
-
-
-    print (greatest([1,2,3,6,0,8]))
-    # >>>8
-
-
-    print (greatest([]))
-    #>>>0
+    assert greatest([4,23,1]) == 23
+    assert greatest([4,23,1,50]) == 50
+    assert greatest([67,99,1,5,200]) == 200
+    assert greatest([1,2,3,6,0,8]) == 8
+    assert greatest([]) == 0
