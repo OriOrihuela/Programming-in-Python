88190b528ad33be1ab695d9b56489daad39cce64
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 15:12:18 +0100

    Update numbers_in_range_matrix.py

--- Identity matrix/numbers_in_range_matrix.py
+++ Identity matrix/numbers_in_range_matrix.py
@@ -10,91 +10,51 @@ def numerosEnRango(matriz):
 if __name__ == "__main__":
 
 
-    correct = [[0,0,0],
-            [1,0,1],
-            [0,1,1]]
+    correct = [[1,2,3],
+            [2,3,1],
+            [3,1,2]]
+    assert numerosEnRango(correct) == True
+    
+    
+    correct2 = [[1,2,3,4],
+                [2,3,1,2],
+                [4,1,2,3],
+                [2,3,1,4]]
+    assert numerosEnRango(correct2) == True
 
-
-    if numerosEnRango(correct):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
+    
     incorrect = [[1,2,3,4],
                 [2,3,1,3],
                 [3,1,2,3],
                 [4,4,4,2]]
+    assert numerosEnRango(incorrect) == False
 
 
-    if numerosEnRango(incorrect):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
-    incorrect2 = [[1,1,1,1],
-                [0,0,0,0],
-                [1,0,1,0],
-                [1,1,0,0]]
-
-
-    if numerosEnRango(incorrect2):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
-    incorrect3 = [[1,2,3,4,5],
+    incorrect2 = [[1,2,3,4,5],
                 [2,3,1,5,6],
                 [4,5,2,1,3],
                 [3,4,5,2,1],
                 [5,6,4,3,2]]
+    assert numerosEnRango(incorrect2) == False
 
-
-    if numerosEnRango(incorrect3):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
-    incorrect4 = [['a','b','c'],
+    
+    incorrect3 = [['a','b','c'],
                 ['b','c','a'],
                 ['c','a','b']]
+    assert numerosEnRango(incorrect3) == False
 
 
-    if numerosEnRango(incorrect4):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
-    incorrect5 = [ [1, 1.5],
+    incorrect4 = [ [1, 1.5],
                 [1.5, 1]]
-
-
-    if numerosEnRango(incorrect5):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
+    assert numerosEnRango(incorrect4) == False
 
         
-    irregular = [[1,2,3],
+    incorrect5 = [[1,2,3],
                 [2,3,1]]
+    assert numerosEnRango(incorrect5) == False
 
 
-    if numerosEnRango(irregular):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
-
-
-    irregular2 = [[1,2,3],
+    incorrect6 = [[1,2,3],
                 [2,3,1],
                 [3,1]]
-
-
-    if numerosEnRango(irregular2):
-        print("Este caso test es correcto")
-    else :
-        print("Este caso test es incorrecto")
+    assert numerosEnRango(incorrect6) == False
