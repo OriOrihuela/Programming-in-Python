37f5d33c11e77c367efba74e8059da5d80655c57
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-06 21:43:49 +0100

    Class GildedRose created

---
+++ GildedRose_kata/gildedRose.py
@@ -0,0 +1,8 @@
+class GildedRose():
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
