94d0d2f8b05a4905e461370da04cf10e3cd8ece5
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-06 23:38:12 +0100

    Def's increaseDate and printDate yet to resolve

--- POO_Exercises/data.py
+++ POO_Exercises/data.py
@@ -1,13 +1,12 @@
 class Data():
     
     def __init__(self, day, month, year):
-        assert day == int and month == int and year == int
-        assert day in range(1,32) and month in range(1,13) and year in range(1900, 3001)
+        assert self.day in range(1,32) and self.month in range(1,13) and self.year in range(1900, 3001)
         try:
             self.day = 1
             self.month = 1
             self.year = 1900
-        except ValueError:
+        except AssertionError:
             self.day = 1
             self.month = 1
             self.year = 1900
@@ -28,3 +27,37 @@ class Data():
     def getYear(self, year):
         return self.year
     
+    def setDate(self, day, month, year):
+        assert self.day in range(1,32) and self.month in range(1,13) and self.year in range(1900, 3001)
+        try:
+            self.day = day
+            self.month = month
+            self.year = year
+        except AssertionError:
+            self.day = 1
+            self.month = 1
+            self.year = 1900
+    
+    def increaseDate(self, number):
+        try:
+            self.day += number
+        except AssertionError:
+            if self.day > 31:
+                try:
+                    self.day = 1
+                    self.month += 1
+                except AssertionError:
+                    if self.month > 12:
+                        self.month = 1
+                        self.year += 1
+
+    def printNameMonth(self, month):
+        values = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        months = dict(zip(keys,values))
+        if self.month in months:
+            self.month = months[keys]
+
+    def  printDate(self, day, month, year):
+        return str(self.day) + ":" + printNameMonth(self.month) + ":" + str(self.year)
+            
