0c725ed05902ced77c5841a3a1667e28c9cb68b6
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-06 21:51:33 +0100

    Created NormalItem Class

---
+++ GildedRose_kata/normal_Item.py
@@ -0,0 +1,28 @@
+from item import Item
+from interface import Interface
+
+class NormalItem(Item, Interface):
+    # Herencia múltiple
+
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def setSell_in(self):
+        self.sell_in = self.sell_in - 1
+
+    def setQuality(self, valor):
+        if self.quality + valor > 50:
+            self.quality = 50
+        elif self.quality + valor >= 0:
+            self.quality = self.quality + valor
+        else:
+            self.quality = 0
+        assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
+
+    # Override metodo update_quality de la interfaz
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.setQuality(-1)
+        else:
+            self.setQuality(-2)
+        self.setSell_in()
