a2301b9a76e0cdc034be8e836ff7d516dd7040a6
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-08 10:52:16 +0100

    Def __init__ created

--- POO_Exercises/hour.py
+++ POO_Exercises/hour.py
@@ -1,2 +1,14 @@
 class Hour():
-    
+    def __init__(self, hour, minutes, seconds):
+        assert self.hour in range(0, 25) and self.minutes in range(0, 60) and self.seconds in range(0, 60)
+        self.hour = 0
+        self.minutes = 0
+        self.seconds = 0
+        try:
+            self.hour = hour
+            self.miutes = minutes
+            self.seconds = seconds
+        except AssertionError:
+            self.hour = 0
+            self.minutes = 0
+            self.seconds = 0
