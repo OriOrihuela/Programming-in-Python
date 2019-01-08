15e753ceee03bc7e8ac28bb56e041caab2bf2442
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-08 17:38:19 +0100

    Defs getters created and test cases

--- GildedRose_kata/item.py
+++ GildedRose_kata/item.py
@@ -1,9 +1,28 @@
-class Item():
+from interface import Interface
+
+class Item(Interface):
 
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
+    def getName(self):
+        return self.name
+    def getSellIn(self):
+        return self.sell_in
+    def getQuality(self):
+        return self.quality
+
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+    new_item = Item("Edu", 10, 100)
+    assert new_item.getName() == "Edu"
+    assert new_item.getSellIn() == 10
+    assert new_item.getQuality() == 100
