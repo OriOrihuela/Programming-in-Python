e2fe086c5bd6e37d5fb7e57b201d8d6c8e8d9cf5
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-08 11:06:30 +0100

    Def setHour created (previous one was an error)

--- POO_Exercises/hour.py
+++ POO_Exercises/hour.py
@@ -29,4 +29,13 @@ class Hour():
         return self.seconds
     
 
-    
+    def setHour(self, hour, minutes, seconds):
+        assert self.hour in range(0, 25) and self.minutes in range(0, 60) and self.seconds in range(0, 60)
+        try:
+            self.hour = hour
+            self.minutes = minutes
+            self.seconds = seconds
+        except AssertionError:
+            self.hour = 0 
+            self.minutes = 0
+            self.seconds = 0
