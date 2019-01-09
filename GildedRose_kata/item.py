a5074b06118cd08b98c75193e69a2cbac814edd2
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-09 19:12:20 +0100

    Getters removed from class Item

--- GildedRose_kata/item.py
+++ GildedRose_kata/item.py
@@ -5,22 +5,10 @@ class Item():
         self.sell_in = sell_in
         self.quality = quality
 
-    def getName(self):
-        return self.name
-    def getSellIn(self):
-        return self.sell_in
-    def getQuality(self):
-        return self.quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.getName(), self.getSellIn(), self.getQuality)
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 if __name__ == "__main__":
     
-
-    # TEST CASES #
-
-    new_item = Item("Edu", 10, 100)
-    assert new_item.getName() == "Edu"
-    assert new_item.getSellIn() == 10
-    assert new_item.getQuality() == 100
+    Edu = Item("Edu", 5, 20)
