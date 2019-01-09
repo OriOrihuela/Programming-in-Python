c242c8acfc16b6db98f3ff373851f30663d7a3e1
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-09 19:11:21 +0100

    Class AgedBrie done

--- GildedRose_kata/agedBrie.py
+++ GildedRose_kata/agedBrie.py
@@ -2,42 +2,22 @@ from normal_Item import NormalItem
 
 class AgedBrie(NormalItem):
 
-    def __init__(self, name, sell_in, quality):
-        NormalItem.__init__(self, name, sell_in, quality)
-    
-    # Override metodo setQuality de NormalItem
-    def setQuality(self, valor):
-
-        if self.quality + valor <= 50:
-            self.quality = self.quality + valor
-        else:
-            self.quality = 50
-        # NormalItem.setQuality(self, valor)
-        assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
-
-    # Override metodo update_quality de la Interfaz
     def update_quality(self):
 
-        if self.sell_in > 0:
-            self.setQuality(1)
+        if self.getSellIn() >= 0:
+            self.quality += 1
+        
         else:
-            self.setQuality(2)
-        self.setSell_in()
-
-
+            self.quality += 2
 
 if __name__ == '__main__':
 
-    item = AgedBrie("Aged Brie", 2, 0)
-    print(item)
+    # TEST CASES #
 
-    # test update_quality
-    for dia in range(1, 10):
-        item.update_quality()
-        print(item)
+    Edu = AgedBrie("Edu", 5, 20)
+    Edu.update_quality()
 
-    # test update_quality incorret input
-    item = AgedBrie("Aged Brie", 2, 100)
-    for dia in range(1, 10):
-        item.update_quality()
-        print(item)
+    assert Edu.getQuality() == 21
+    assert Edu.getSellIn() == 5
+
+    
