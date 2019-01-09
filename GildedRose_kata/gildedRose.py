91449a2309d865c3365f684d7e4443a386d999f4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-09 20:20:03 +0100

    Class GildedRose done

--- GildedRose_kata/gildedRose.py
+++ GildedRose_kata/gildedRose.py
@@ -1,8 +1,34 @@
+from sulfuras import Sulfuras
+from agedBrie import AgedBrie
+from backstage import Backstage
+
+
 class GildedRose():
 
-    def __init__(self, items):
-        self.items = items
+    def __init__(self, stock):
+        self.stock = stock
 
-    def update_quality(self):
-        for item in self.items:
+    
+    def getStock(self):
+        return self.stock
+    
+
+    def updateStock(self):
+        for item in self.stock:
             item.update_quality()
+
+
+
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+    stock = [Sulfuras('sulfuras', 10, 80), AgedBrie('agedBrie', 3, 4), Backstage('backstage', 5, 25)]
+    tienda = GildedRose(stock)
+    tienda.updateStock()
+
+    assert tienda.getStock()[0].getQuality() == 80
+    assert tienda.getStock()[0].getName() == "sulfuras"
+    assert tienda.getStock()[0].getSellIn() == 10
