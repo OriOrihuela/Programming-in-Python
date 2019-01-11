10bfd407f4daac5d59a07d9cac3f43a4b258732d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-11 12:43:49 +0100

    Class ConjuredItem done

--- GildedRose_kata/conjured_item.py
+++ GildedRose_kata/conjured_item.py
@@ -2,14 +2,22 @@ from normal_Item import NormalItem
 
 class ConjuredItem(NormalItem):
 
-    def __init__(self, name, sell_in, quality):
-        NormalItem.__init__(self, name, sell_in, quality)
-
-    # Override metodo update_quality de la Interfaz
-    # Heredado a traves de NormalItem
     def update_quality(self):
         if self.sell_in >= 0:
             self.setQuality(-2)
         else:
             self.setQuality(-4)
-        self.setSell_in()
+        self.setSell_In()
+
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+    new_item = ConjuredItem("Edu", 5, 25)
+    new_item.update_quality()
+
+    assert new_item.getQuality() == 23
+    assert new_item.getSellIn() == 4
+    assert new_item.getName() == "Edu"
