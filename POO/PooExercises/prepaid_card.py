b0c9ddf9cc9108b6cc715d10fe4115b28e739b34
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-11 13:36:35 +0100

    Class and Def __init__ created

---
+++ POO/PooExercises/prepaid_card.py
@@ -0,0 +1,7 @@
+class PrepaidCard():
+
+    def __init__(self, phone, balance, consumption):
+        self.phone = phone
+        self.balance = balance
+        self.consumption = consumption
+        
