16b709eabe47223173d9f7ed6e18b39f484e08f5
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-14 21:03:15 +0100

    Class almost done

--- POO/PooExercises/prepaid_card.py
+++ POO/PooExercises/prepaid_card.py
@@ -1,7 +1,28 @@
 class PrepaidCard():
 
-    def __init__(self, phone, balance, consumption):
+    def __init__(self, phone, dni, balance, consumption):
         self.phone = phone
+        self.dni = dni
         self.balance = balance
         self.consumption = consumption
         
+
+    def setPhone(self, phone):
+        self.phone = phone
+    def getPhone(self):
+        return self.phone
+    
+    def setDni(self, dni):
+        self.dni = dni
+    def getDni(self):
+        return self.dni
+
+    def setBalance(self, balance):
+        self.balance = balance
+    def getBalance(self):
+        return self.balance
+    
+    def setConsumption(self, consumption):
+        self.consumption = consumption
+    def getConsumption(self):
+        return self.consumption
