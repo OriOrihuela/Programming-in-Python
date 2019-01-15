6c2564a19ee5f8f8b1da178e841f4fdc00bef9b6
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-15 13:13:45 +0100

    Class almost done, yet to resolve consumption in makeCall()

--- POO/PooExercises/prepaid_card.py
+++ POO/PooExercises/prepaid_card.py
@@ -1,10 +1,12 @@
-class PrepaidCard():
+from hour import Hour
 
-    def __init__(self, phone, dni, balance, consumption):
+class PrepaidCard(Hour):
+
+    def __init__(self, phone, dni, balance):
         self.phone = phone
         self.dni = dni
         self.balance = balance
-        self.consumption = consumption
+        assert isinstance(self.phone, str) and isinstance(self.dni, str) and isinstance(self.balance, int)
         
 
     def setPhone(self, phone):
@@ -17,15 +19,8 @@ class PrepaidCard():
     def getDni(self):
         return self.dni
 
-    def setBalance(self, balance):
-        self.balance = balance
     def getBalance(self):
         return self.balance
-    
-    def setConsumption(self, consumption):
-        self.consumption = consumption
-    def getConsumption(self):
-        return self.consumption
     
 
     def depositBalance(self, amount):
@@ -36,5 +31,26 @@ class PrepaidCard():
         cost = numberMessagesToSend * 0.09
         self.balance -= cost
     
+    
+    def makeCall(self, seconds):
+        cost = 15 + (seconds * 0.01)
+        self.balance -= cost
 
     
+
+    def checkCard(self):
+        for attribute in (self.__dict__):
+            print("%s:%s" % (attribute, self.__dict__[attribute]))
+
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+
+    new_card = PrepaidCard("555666444", "98765432V", 500)
+    new_card.depositBalance(500)
+    new_card.sendMessage(10)
+    assert new_card.getBalance() == 999.1
+    assert new_card.getDni() == "98765432V"
